979d2f122980d7ccc3b42b9b90633af29cceee3e
Akash_96 <realakash9630@gmail.com>
2022-11-29 07:39:50 +0530

    alter

--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -4,10 +4,11 @@ import { Grid } from "@mui/material";
 import VideoItem from "./VideoItem";
 
 const VideoList = ({ videos, onVideoSelect }) => {
-    const listOfVideos = videos.map(video => (
+    const listOfVideos = videos.map((video, index) => (
         <VideoItem
             onVideoSelect={onVideoSelect}
-            key={video.id.videoId}
+            // key={video.id.videoId}
+            key={index}
             video={video}
         />
     ));
